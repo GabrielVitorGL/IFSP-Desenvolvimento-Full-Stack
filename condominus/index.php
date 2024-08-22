085eab67eb40a98d3346c939cb94466f432119b4
CTI Guarulhos <ctiguarulhos@ifsp.edu.br>
2024-08-21 22:02:38 -0300

    adding condominus project files

---
+++ condominus/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Condominus</title>
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href="./adminlte/plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="./adminlte/dist/css/adminlte.min.css">
+</head>
+
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+        <?php include('./includes/component/navbar.php') ?>
+        <?php include('./includes/component/sidebar.php') ?>
+
+        <div class="content-wrapper">
+            <?php include('./includes/component/breadcrumb.php') ?>
+            <?php include('./includes/component/maincontent.php') ?>
+        </div>
+
+        <?php include('./includes/component/footer.php') ?>
+
+        <aside class="control-sidebar control-sidebar-dark"></aside>
+    </div>
+
+    <script src="./adminlte/plugins/jquery/jquery.min.js"></script>
+    <script src="./adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="./adminlte/dist/js/adminlte.min.js"></script>
+    <script src="./adminlte/dist/js/demo.js"></script>
+</body>
+
+</html>
